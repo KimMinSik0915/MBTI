1812e39a282dad824cc45f2834e05a9dee739a46
한국세무사회 <@DESKTOP-IPG2JCP>
2021-03-23 15:42:38 +0900

    멤버 쿼리 추가

--- src/com/mbti/util/db/DBSQL.java
+++ src/com/mbti/util/db/DBSQL.java
@@ -2,7 +2,7 @@ package com.mbti.util.db;
 
 public class DBSQL {
 //=======================================================================
-	//게시판 쿼리 작성
+	//게시판 쿼리 작성 --------------------------------------------------------
 	//1. 게시판 리스트
 	public static final String BOARD_LIST
 	= " select rnum, no, title, id, "
@@ -41,7 +41,7 @@ public class DBSQL {
 	= " delete from board where no = ? ";
 	
 	//=======================================================================
-	// 공지사항 쿼리
+	// 공지사항 쿼리 -------------------------------------------------------------
 	// 1. 공지사항 리스트 
 	public static final String NOTICE_LIST
 	= " select rnum, no, title, "
@@ -72,7 +72,7 @@ public class DBSQL {
 	= " delete from notice where no = ? ";
 	
 	//=======================================================================
-	// 유형 게시판 쿼리 ------------------------------------
+	// 유형 게시판 쿼리 -----------------------------------------------------------
 	//1. 리스트 - 번호, 유형, 이미지, 수정일
 	public static final String TYPE_LIST
 	
@@ -106,6 +106,47 @@ public class DBSQL {
 	//5.유형 이미지 삭제 
 	public static final String TYPE_DELETE
 	= " delete from type where no = ? ";
+	//=======================================================================
+	// 회원관리 쿼리 -------------------------------------------------------------
+	// 로그인 처리
+	public static final String MEMBER_LOGIN
+	= " select m.id, m.name, m.gradeNo, g.gradeName from member m, grade g "
+	+ " where (m.id = ? and m.pw = ?) and (m.gradeNo = g.gradeNo) ";
+	
+	// 회원 리스트 처리 - id, name, gender, birth, tel, status, gradeNo, gradeName
+	public static final String MEMBER_LIST 
+	= " select rnum, id, name, "
+	+ " tel, status, gradeNo, gradeName from( "
+		+ " select rownum rnum, id, name, tel, status, "
+		+ " gradeNo, gradeName from ( "
+			+ " select m.id, m.name, m.tel, m.status, "
+			+ " m.gradeNo, g.gradeName "
+			+ " from member m, grade g "
+			+ " where m.gradeNo = g.gradeNo "
+			+ " order by id "
+		+ " ) "
+	+ ") where rnum between ? and ?  ";
+	
+	// 회원 리스트 처리 - id, name, gender, birth, tel, status, gradeNo, gradeName
+	public static final String MEMBER_VIEW
+	= " select id, name, tel, email, " +
+	// to_char할때 (yy...)하고 나서 뒤에 m. 같은 줄임표시 치면 에러뜸 
+			" status " + 
+			" from member " + 
+			" where id = ? ";
+	// 회원가입
+	public static final String MEMBER_WRITE
+	= " insert into member(id, pw, name, tel, email ) "
+	+ " values(?, ?, ?, ?, ?, ?, ?) ";
+	// 아이디 중복 체크
+	public static final String MEMBER_CHECK_ID
+	= "select id from member where id = ?";
+	
+	public static final String MEMBER_GRADE_MODIFY
+	= "update member set gradeNo = ? where id = ?";
+
 }
+
+
 
 
