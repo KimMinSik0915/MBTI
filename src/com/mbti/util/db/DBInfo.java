e4efe9f128e5a3ddbcb92484196706c997381437
한국세무사회 <@DESKTOP-IPG2JCP>
2021-03-23 11:35:40 +0900

    DB쪽 ip 수정

--- src/com/mbti/util/db/DBInfo.java
+++ src/com/mbti/util/db/DBInfo.java
@@ -20,7 +20,7 @@ public class DBInfo {
 	private static boolean driverCheck = false;
 	
 	// 1-1 DB에 대한 접속 정보
-	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String URL = "jdbc:oracle:thin:@192.168.0.125:1521:xe";
 	
 	private static final String ID = "team1";
 	
