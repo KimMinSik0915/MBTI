994e2008fe82943cc587d25ccc26a3a01ae3e636
EZEN <EZEN@DESKTOP-IPG2JCP>
2021-03-26 14:24:43 +0900

    2021.03.26 Init 파일 수정 	1. type/deleteService 추가 	2. syso test 수정

--- src/com/mbti/main/controller/Init.java
+++ src/com/mbti/main/controller/Init.java
@@ -47,6 +47,7 @@ import com.mbti.notice.service.NoticeWriteService;
 import com.mbti.result.controller.ResultController;
 import com.mbti.type.controller.TypeController;
 import com.mbti.type.dao.TypeDAO;
+import com.mbti.type.service.TypeDeleteService;
 import com.mbti.type.service.TypeListService;
 import com.mbti.type.service.TypeUpdateFileService;
 import com.mbti.type.service.TypeViewService;
@@ -110,12 +111,14 @@ public class Init extends HttpServlet {
 		Beans.putService("/type/view.do", new TypeViewService());
 		Beans.putService("/type/write.do", new TypeWriteService());
 		Beans.putService("/type/update.do", new TypeUpdateFileService());
+		Beans.putService("/type/delete.do", new TypeDeleteService());
 		
 		//service에 dao 넣기
 		Beans.getService("/type/list.do").setDAO(Beans.getDAO("typeDAO"));
 		Beans.getService("/type/view.do").setDAO(Beans.getDAO("typeDAO"));
 		Beans.getService("/type/write.do").setDAO(Beans.getDAO("typeDAO"));
 		Beans.getService("/type/update.do").setDAO(Beans.getDAO("typeDAO"));
+		Beans.getService("/type/delete.do").setDAO(Beans.getDAO("typeDAO"));
 		
 		// Service, Controller, DAO를 저장할 때 오탈자 꼭 확인하고 Service는 꼭 DAO를 넣었는지 확인할 것!!!!
 		// Board ==========================================================================
@@ -192,8 +195,8 @@ public class Init extends HttpServlet {
 		
 		// 저장이 잘 되어 있는지 확인
 //		System.out.println("Init.init().Beans.getCotroller(\"/member :  " + Beans.getController("/member"));
-		System.out.println("Init.init().Beans.getService(\"/member/delete.do\" :  " + Beans.getService("/member/delete.do"));
-		System.out.println("Init.init().Beans.get(\"/member/delete.do\" :  " + Beans.getDAO("/member/delete.do"));
+		System.out.println("Init.init().Beans.getService(\"/type/delete.do\" :  " + Beans.getService("/type/delete.do"));
+		System.out.println("Init.init().Beans.getDAO(\"/type/delete.do\" :  " + Beans.getDAO("typeDAO"));
 		
 		try {
 			
