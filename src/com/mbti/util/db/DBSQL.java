2d31d143468049cb5e1db28764441de67232c043
한국세무사회 <@DESKTOP-IPG2JCP>
2021-03-26 14:19:06 +0900

    DBSQL - MEMBER 추가

--- src/com/mbti/util/db/DBSQL.java
+++ src/com/mbti/util/db/DBSQL.java
@@ -112,7 +112,7 @@ public class DBSQL {
 	// 회원관리 쿼리 -------------------------------------------------------------
 	// 1. 로그인 처리
 	public static final String MEMBER_LOGIN
-	= " select id, name, gradeNo, gradeName from member m, grade g "
+	= " select m.id, m.name, m.gradeNo, g.gradeName from member m, grade g "
 	+ " where (m.id = ? and m.pw = ?) and (m.gradeNo = g.gradeNo) ";
 	
 	// 2. 회원가입 처리
@@ -135,7 +135,7 @@ public class DBSQL {
 	// 4. 회원 리스트
 	public static final String MEMBER_LIST
 	= " select rnum, id, name, gender, "
-	+ "to_char(birth, 'yyyy.mm.dd' birth,m tel, status, gradeNo, gradeName from( "
+	+ "to_char(birth, 'yyyy.mm.dd') birth, tel, status, gradeNo, gradeName from( "
 		+ " select rownum rnum, id, name, gender, birth, tel, status, "
 		+ " gradeNo, gradeName from( "
 			+ " select m.id, m.name, m.gender, m.birth, m.tel, m.status, "
@@ -149,6 +149,11 @@ public class DBSQL {
 	// 5. 회원 등급 수정
 	public static final String MEMBER_GRADE_MODIFY
 	= "update member set gradeNo = ? where id = ?";
+	
+	// 6. 회원 탈퇴 처리
+	public static final String MEMBER_DELETE 
+	= " delete from member where id = ? ";
+
 	//=======================================================================
 	// 테스트 목록 쿼리 -----------------------------------------------------------
 	public static final String LIST_LIST
